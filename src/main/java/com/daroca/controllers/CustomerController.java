52315ffb2453a2f7a59fcd932b62fd32332eba5c
Polabiel <bielgabrieloliveira77@gmail.com>
2024-06-04 22:14:59 -0300

    Add: new implementaions in code with Customer.java and SalesOrderItem.java

--- src/main/java/com/daroca/controllers/CustomerController.java
+++ src/main/java/com/daroca/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -13,12 +14,36 @@ import java.util.Optional;
 public class CustomerController {
     @GetMapping
     public List<Customer> getCustomers() {
-
         return null;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomerController that)) return false;
+        return Objects.equals(repository, that.repository);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerController{" +
+                "repository=" + repository +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(repository);
     }
 
     @Autowired
     private CustomerRepository repository;
+
+    @GetMapping("/customers")
+    public List<Customer> All() {
+        return repository.findAll();
+    }
 
     @GetMapping("/customers/{id}")
     public Optional<Customer> getById(@PathVariable Integer id) {
